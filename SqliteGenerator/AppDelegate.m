59431c80039cd237c18181ec4355757f13766e1a
ceairdev <ceairdev@ceairdevmatoiMac.local>
2011-12-27 09:04:30 +0800

    Initial Commit

---
+++ SqliteGenerator/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  SqliteGenerator
+//
+//  Created by  on 11-12-27.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ SqliteGenerator/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  SqliteGenerator
+//
+//  Created by  on 11-12-27.
+//  Copyright (c) 2011年 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
